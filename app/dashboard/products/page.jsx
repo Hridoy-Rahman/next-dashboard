4e29f4a758fdffc73f5e1af5fb4e02c1bb6e2126
Hridoy-Rahman <hridoyrahman715@gmail.com>
2024-03-03 23:12:04 +0600

    products page created

---
+++ app/dashboard/products/page.jsx
@@ -0,0 +1,62 @@
+import Search from "@/app/ui/dashboard/search/search";
+import Link from "next/link";
+import React from "react";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Image from "next/image";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import View from "@/app/ui/dashboard/buttons/view";
+import Delete from "@/app/ui/dashboard/buttons/delete";
+
+const UsersPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for user" />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  className="rounded-full"
+                  src="/noitem.jpg"
+                  alt="product image"
+                  width="40"
+                  height="40"
+                ></Image>
+                iPhone 11
+              </div>
+            </td>
+            <td>Desc</td>
+            <td>$120.50</td>
+            <td>03.03.2024</td>
+            <td>32</td>
+            <td className="flex gap-2">
+              <Link href="/">
+                <View/>
+              </Link>
+              <Delete/>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination/>
+    </div>
+  );
+};
+
+export default UsersPage;
