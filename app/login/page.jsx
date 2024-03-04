8ce268d89787a98a92c4ae40851bbea88ea9ce42
Hridoy-Rahman <hridoyrahman715@gmail.com>
2024-03-04 15:14:48 +0600

    login page created

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+"use client";
+import React, { useState } from "react";
+import styles from "@/app/ui/login/login.module.css";
+import Button from "@/app/ui/dashboard/buttons/button";
+import { FaEyeSlash,FaEye  } from "react-icons/fa";
 
-const login = () => {
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div>
-      Login
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <input type="text" placeholder="Username" />
+        <div className="flex gap-2 items-center">
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+          />
+          <button
+            type="button"
+            onClick={togglePasswordVisibility}
+          >
+            {showPassword ? <FaEyeSlash /> : <FaEye  />}
+          </button>
+        </div>
+        <Button customClass="bg-teal-500 p-2 rounded-lg w-full" name="Login" />
+      </form>
     </div>
   );
 };
 
-export default login;
+export default Login;
