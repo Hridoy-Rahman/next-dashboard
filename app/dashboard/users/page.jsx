3d239c4c6b412c874936edabd5bb363e9fac519b
Hridoy-Rahman <hridoyrahman715@gmail.com>
2024-03-04 22:53:23 +0600

    fetch data from database

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -7,10 +7,9 @@ import Pagination from "@/app/ui/dashboard/pagination/pagination";
 import Button from "@/app/ui/dashboard/buttons/button";
 import { fetchUsers } from "@/app/lib/data";
 
-const UsersPage = async() => {
-
+const UsersPage = async () => {
   const users = await fetchUsers();
-  console.log(users)
+  console.log(users);
 
   return (
     <div className={styles.container}>
@@ -32,38 +31,40 @@ const UsersPage = async() => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <div className={styles.user}>
-                <Image
-                  className="rounded-full"
-                  src="/images.png"
-                  alt="user image"
-                  width="40"
-                  height="40"
-                ></Image>
-                Jhon Doe
-              </div>
-            </td>
-            <td>jhondoe@gmail.com</td>
-            <td>03.03.2024</td>
-            <td>Admin</td>
-            <td>Active</td>
-            <td className="flex gap-2">
-              <Link href="/dashboard/users/test">
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    className="rounded-full"
+                    src={user.img || "/images.png"}
+                    alt="user image"
+                    width="40"
+                    height="40"
+                  ></Image>
+                  {user.username}
+                </div>
+              </td>
+              <td>{user.email}</td>
+              <td>{user.createdAt?.toString().slice(4,16)}</td>
+              <td>{user.isAdmin ? "Admin" : "Client"}</td>
+              <td>{user.isActive ? "Active" : "Inactive"}</td>
+              <td className="flex gap-2">
+                <Link href={`/dashboard/users/${user.id}`}>
+                  <Button
+                    buttonType="button"
+                    customClass="bg-teal-500 p-1 rounded-md"
+                    name="View"
+                  />
+                </Link>
                 <Button
-                  buttonType="button"
-                  customClass="bg-teal-500 p-1 rounded-md"
-                  name="View"
-                />
-              </Link>
-              <Button
                   buttonType="button"
                   customClass="bg-red-600 p-1 rounded-md"
                   name="Delete"
                 />
-            </td>
-          </tr>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <Pagination />
