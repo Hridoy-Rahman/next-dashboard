1e425752b4b7758658f0cf88bdb3a3f14e29e888
Hridoy-Rahman <hridoyrahman715@gmail.com>
2024-03-04 11:08:16 +0600

    add user page created

---
+++ app/dashboard/users/add/page.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "@/app/ui/dashboard/users/add/addUsers.module.css";
+
+const AddUsers = () => {
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <input type="text" placeholder="username" name="username" required />
+        <input type="email" placeholder="email" name="email" required />
+        <input type="password" placeholder="password" name="password" required />
+        <input type="number" placeholder="phone" name="phone" required />
+        <select name="isAdmin" id="isAdmin">
+          <option value={false}>Is Admin</option>
+          <option value={true}>Yes</option>
+          <option value={false}> No</option>
+        </select>
+        <select name="isActive" id="isActive">
+          <option value={true}>Is Active</option>
+          <option value={true}>Yes</option>
+          <option value={false}> No</option>
+        </select>
+        <textarea
+          name="address"
+          id="address"
+          rows="10"
+          placeholder="Address"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddUsers;
--- app/ui/dashboard/footer/footer.jsx
+++ app/ui/dashboard/footer/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='justify-center text-sm'>
+        <div className='justify-center text-sm mt-10'>
             <div className='font-bold text-center pb-1'>
                 Hridoy Rahman
                 @All right reserved
